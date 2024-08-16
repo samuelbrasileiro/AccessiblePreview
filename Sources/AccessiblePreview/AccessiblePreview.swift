b3fccea0c9b25612f0cfc417c96e7d8a3d716e7c
Samuel <samuelbsantosn@gmail.com>
2024-08-16 10:41:53 -0300

    Add macro naming conformance

--- Sources/AccessiblePreview/AccessiblePreview.swift
+++ Sources/AccessiblePreview/AccessiblePreview.swift
@@ -15,15 +15,8 @@ public enum AccessibilityTrait {
 }
 @available(iOS 17.0, macOS 14.0, tvOS 17.0, visionOS 1.0, watchOS 10.0, *)
 @freestanding(declaration, names: named(Previews))
-public macro Preview(trait: AccessibilityTrait, name: String? = nil, body: @escaping @MainActor () -> any View) = #externalMacro(module: "AccessiblePreviewMacroMacros", type: "AccessibilityPreviewMacro")
+public macro Preview(trait: AccessibilityTrait, name: String? = nil, body: @escaping @MainActor () -> any View) = #externalMacro(module: "AccessiblePreviewMacros", type: "AccessibilityPreviewMacro")
 
 @available(iOS 17.0, macOS 14.0, tvOS 17.0, visionOS 1.0, watchOS 10.0, *)
 @freestanding(declaration, names: named(Previews))
-public macro Preview(traits: AccessibilityTrait..., name: String? = nil, body: @escaping @MainActor () -> any View) = #externalMacro(module: "AccessiblePreviewMacroMacros", type: "AccessibilityPreviewMacro")
-
-@freestanding(expression)
-public macro Accessible(_ body: @escaping @MainActor () -> any View) -> any View = #externalMacro(module: "AccessiblePreviewMacroMacros", type: "AccessibleMacro")
-
-@freestanding(declaration)
-@attached(extension, conformances: View)
-public macro Accessible() = #externalMacro(module: "AccessiblePreviewMacroMacros", type: "AccessibleMacro")
+public macro Preview(traits: AccessibilityTrait..., name: String? = nil, body: @escaping @MainActor () -> any View) = #externalMacro(module: "AccessiblePreviewMacros", type: "AccessibilityPreviewMacro")
